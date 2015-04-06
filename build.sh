80ec8f5bdf87241b737b535d4e40ca1a0723738a
mastero9017 <mastero9017@gmail.com>
2015-04-06 22:07:37 +0200

    Welcome permissive

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ cd ../ramdisk_hammerhead/
 echo "making ramdisk"
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
 echo "making boot image"
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot.img
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1 androidboot.selinux=permissive' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot.img
 
 rm -rf ramdisk.gz
 rm -rf zImage-dtb
